1c69315524aef11a346036a8ad7b77725f084eba
fade697 <132776309+fade697@users.noreply.github.com>
2023-08-29 14:01:58 +0300

    three

---
+++ three_problem_solving/three.js
@@ -0,0 +1,13 @@
+function revers(str){
+    const str1 = [];
+    const revers2=null;
+    str1=str.split();
+    for (let i=str1.length;i>0;i--){
+        revers2=revers2+str1[1];
+    }
+
+
+}
+const input ="Hello word!";
+const sum=revers(input);
+console.log(sum);
